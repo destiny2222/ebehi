88e40f2d8d2c5b467e315a90faa315fea2ba2652
destiny2222 <oghenekumeerieme@gmail.com>
2024-02-15 23:19:31 +0100

    update

--- school/settings.py
+++ school/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'qd=rji6^d60xapks@n%_gi!4xlz%r1*nt)b@zdp9j--jv!l2&*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['schoolauto.pythonanywhere.com', '127.0.0.1']
 # ALLOWED_HOSTS = ['192.168.137.1']
 
 
